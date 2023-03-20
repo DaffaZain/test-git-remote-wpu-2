59740f7aa6fa9e7f4544ddb31259439849727edb
Daffa Andika Zain <99741262+DaffaZain@users.noreply.github.com>
2023-03-20 07:30:06 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hello world</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam veniam, quis non natus nam modi dicta sunt fugiat commodi impedit dolorem asperiores pariatur odio in ratione earum, eligendi, doloribus aperiam?</p>
+</body>
+</html>
