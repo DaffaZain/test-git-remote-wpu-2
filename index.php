f788b13a4bdbb9f3764aa596f58f506e69d364ce
Daffa Andika Zain <99741262+DaffaZain@users.noreply.github.com>
2023-03-20 07:36:28 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam veniam, quis non natus nam modi dicta sunt fugiat commodi impedit dolorem asperiores pariatur odio in ratione earum, eligendi, doloribus aperiam?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
